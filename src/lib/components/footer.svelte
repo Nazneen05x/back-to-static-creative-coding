3850b80bb617be4f5e738700a8e5dc3caac96c29
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-12-05 18:00:49 +0100

    SvelteKit & footer
    Ik heb eerst Sveltekit geïnstalleerd en daarna begonnen met het opzetten van de footer.

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,22 @@
+<footer>
+  <section>
+    <img src="/visual-thinking-logo-footer.svg" alt="footerlogo" />
+  </section>
+
+  <section>
+    <ul>
+      <li>Over</li>
+      <li>Artikelen</li>
+      <li>Tekemethodes</li>
+      <li>Kennisclips</li>
+    </ul>
+
+    <ul>
+      <h4>Contact</h4>
+      <li><img src="/instagram.svg" alt="instagramlogo" /></li>
+      <li><img src="/youtube.svg" alt="youtubelogo" /></li>
+      <li><img src="/linkedin.svg" alt="linkedinlogo" /></li>
+      <h5>© Charley Muhren, 2022</h5>
+    </ul>
+  </section>
+</footer>
