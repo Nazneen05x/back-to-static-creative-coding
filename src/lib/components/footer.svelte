7931df6f13a94ad6e8117aec5113e965521b4e9e
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-12-07 15:07:22 +0100

    done

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,4 +1,6 @@
 <footer>
+  <section class="star1"></section>
+  <section class="star2"></section>
   <img
     class="footerimg"
     src="/visual-thinking-logo-footer.svg"
@@ -29,14 +31,22 @@
 
     <h5>© Charley Muhren, 2022</h5>
   </section>
+
+  <section class="star"></section>
+  <section class="star3"></section>
 </footer>
 
 <style>
   footer {
     margin-top: 1.5em;
-    background-color: #f0f0f0;
     padding: 1.5em;
     margin: -1em;
+    background: rgb(192, 190, 185);
+    background: linear-gradient(
+      90deg,
+      rgba(192, 190, 185, 1) 14%,
+      rgba(9, 9, 64, 1) 54%
+    );
   }
 
   .footerimg {
@@ -54,13 +64,80 @@
     margin: 0em;
     font-family: var(--vtPrimaryFont);
     line-height: 1.5em;
+    color: var(--white);
   }
 
   h4,
   h5 {
-    color: var(--vtDarkBlue);
     font-family: var(--vtPrimaryFont);
     margin-left: 2.5em;
+    color: var(--white);
+  }
+
+  /* SPARKLE ANIMATION */
+  /* JellyPir.@tCoding (codepen) */
+  .star {
+    width: 6px;
+    height: 6px;
+    background-color: #feb51e;
+    border-radius: 50%;
+    opacity: 0;
+    animation: sparkle 4s infinite alternate;
+  }
+
+  .star1 {
+    margin-left: 20em;
+    top: 2em;
+    position: absolute;
+    width: 6px;
+    height: 6px;
+    background-color: #feb51e;
+    border-radius: 50%;
+    opacity: 0;
+    animation: sparkle 4s infinite;
+  }
+
+  .star2 {
+    margin-left: 17em;
+    top: 9em;
+    position: absolute;
+    width: 6px;
+    height: 6px;
+    background-color: #feb51e;
+    border-radius: 50%;
+    opacity: 0;
+    animation: sparkle 4s infinite;
+  }
+
+  .star3 {
+    margin-left: 19em;
+    top: 15em;
+    position: absolute;
+    width: 8px;
+    height: 8px;
+    background-color: #feb51e;
+    border-radius: 50%;
+    opacity: 0;
+    animation: sparkle 4s infinite;
+  }
+
+  @keyframes sparkle {
+    50% {
+      opacity: 0.4;
+      transform: scale(0.5);
+      box-shadow: 0px 0px 10px 5px #feb51e;
+    }
+    50% {
+      opacity: 1;
+      transform: scale(1);
+      box-shadow: 0px 0px 10px 5px #feb51e;
+    }
+
+    100% {
+      opacity: 0;
+      transform: scale(1.5);
+      box-shadow: 0px 0px 10px 5px #feb51e;
+    }
   }
 
   @media only screen and (min-width: 900px) {
@@ -69,6 +146,7 @@
       justify-content: center;
       align-items: center;
       gap: 8em;
+      height: 100%;
     }
 
     .logoimglist {
@@ -83,6 +161,72 @@
 
     .ullinks {
       margin-left: -5em;
+    }
+
+    /* SPARKLE ANIMATION */
+    /* JellyPir.@tCoding (codepen) */
+    .star {
+      width: 8px;
+      height: 8px;
+      background-color: #feb51e;
+      border-radius: 50%;
+      opacity: 0;
+      animation: sparkle 2s infinite alternate;
+    }
+
+    .star1 {
+      margin-left: -18em;
+      top: 2em;
+      position: absolute;
+      width: 6px;
+      height: 6px;
+      background-color: #feb51e;
+      border-radius: 50%;
+      opacity: 0;
+      animation: sparkle 3s infinite;
+    }
+
+    .star2 {
+      margin-left: 9em;
+      top: 9em;
+      position: absolute;
+      width: 6px;
+      height: 6px;
+      background-color: #feb51e;
+      border-radius: 50%;
+      opacity: 0;
+      animation: sparkle 4s infinite;
+    }
+
+    .star3 {
+      margin-left: 80em;
+      top: 9em;
+      position: absolute;
+      width: 8px;
+      height: 8px;
+      background-color: #feb51e;
+      border-radius: 50%;
+      opacity: 0;
+      animation: sparkle 4s infinite;
+    }
+
+    @keyframes sparkle {
+      50% {
+        opacity: 0.4;
+        transform: scale(0.5);
+        box-shadow: 0px 0px 10px 5px #feb51e;
+      }
+      50% {
+        opacity: 1;
+        transform: scale(1);
+        box-shadow: 0px 0px 10px 5px #feb51e;
+      }
+
+      100% {
+        opacity: 0;
+        transform: scale(1.5);
+        box-shadow: 0px 0px 10px 5px #feb51e;
+      }
     }
   }
 </style>
